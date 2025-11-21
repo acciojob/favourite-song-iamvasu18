ecbdac8f4d040dc9211f975167f2974b871e800b
Dharavath Vasu Naik <168115062+iamvasu18@users.noreply.github.com>
2025-11-21 17:32:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+const song_name="Blinding Lights";
+const song_url="https://www.youtube.com/watch?v=4NRXx6U8ABQ";
+const div=document.getElementById("container");
+const ele=document.createElement("p");
+const song=document.createElement("a");
+
+song.textContent=song_name;
+song.href=song_url;
+
+ele.textContent="My current favourite song is ";
+ele.appendChild(song);
+
+div.appendChild(ele);
